9dac5b6df300533d9b0106b04ef19e2f4491a069
Tohid Naikwadi <naikwadimdtohid@gmail.com>
2025-11-02 09:51:17 +0530

    Setuping Websockets and configuration

---
+++ src/main/java/com/example/RydeWebsocketServer/RydeWebsocketServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.RydeWebsocketServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RydeWebsocketServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RydeWebsocketServerApplication.class, args);
+	}
+
+}
