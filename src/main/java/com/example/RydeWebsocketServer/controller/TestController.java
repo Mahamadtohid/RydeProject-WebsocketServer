0a73002f3328eda9e1468dc445fdfdf0afda094b
Tohid Naikwadi <naikwadimdtohid@gmail.com>
2025-11-02 12:15:24 +0530

    ScheduleConfig

--- src/main/java/com/example/RydeWebsocketServer/controller/TestController.java
+++ src/main/java/com/example/RydeWebsocketServer/controller/TestController.java
@@ -1,10 +1,37 @@
 package com.example.RydeWebsocketServer.controller;
 
 
+import com.example.RydeWebsocketServer.dtos.TestRequest;
+import com.example.RydeWebsocketServer.dtos.TestResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class TestController {
 
-    public String pingCheck();
+//    @Autowired
+    private SimpMessagingTemplate messageSender;
+
+    public TestController(SimpMessagingTemplate messageSender){
+        this.messageSender = messageSender;
+    }
+
+    @MessageMapping("/ping")
+    @SendTo("/topic/ping")
+    public TestResponse pingCheck(TestRequest message){
+
+        return new TestResponse("Received");
+    }
+
+    @SendTo("/topic/sheduled")
+    @Scheduled(fixedDelay = 2000)
+    public void periodicMsg(TestRequest message){
+        messageSender.convertAndSend("/topic/sheduled" , "periodic message sent " + System.currentTimeMillis());
+    }
+
+
 }
---
+++ src/main/java/com/example/RydeWebsocketServer/dtos/TestResponse.java
@@ -0,0 +1,14 @@
+package com.example.RydeWebsocketServer.dtos;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TestResponse {
+
+    public String data;
+}
